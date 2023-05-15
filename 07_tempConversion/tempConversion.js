560450d657339f2b0dc6825eea858c0761720454
zichen-3974 <e0773974@u.nus.edu>
2023-05-15 15:14:34 +0800

    Add attempt for ex7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  return Number((5 / 9 * (f - 32)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  return Number((c * 1.8 + 32).toFixed(1));
 };
 
 // Do not edit below this line
