9e048cad1dceb5cbfc52a5adca0925ddb154d7c2
zichen-3974 <e0773974@u.nus.edu>
2023-05-15 15:02:24 +0800

    Add attempt for ex5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,8 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || typeof a !== 'number' || typeof b !== 'number') {
+        return 'ERROR';
+    }
+    return (a + b) * (Math.abs(b - a) + 1) / 2;
 };
 
 // Do not edit below this line
